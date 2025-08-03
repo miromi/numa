2daddcbc4c72d5ae91a41677a981a53641347de9
Ray <miromi1983@gmail.com>
2025-08-03 12:05:24 +0800

    fix: 在CLI中注册问题命令

--- numa_cli/main.py
+++ numa_cli/main.py
@@ -1,5 +1,5 @@
 import click
-from app.commands import requirements, solutions, development, deployment, users, applications
+from app.commands import requirements, solutions, development, deployment, users, applications, questions
 
 @click.group()
 def cli():
@@ -12,6 +12,7 @@ cli.add_command(development.development)
 cli.add_command(deployment.deployment)
 cli.add_command(users.users)
 cli.add_command(applications.applications)
+cli.add_command(questions.questions)
 
 if __name__ == '__main__':
     cli()
